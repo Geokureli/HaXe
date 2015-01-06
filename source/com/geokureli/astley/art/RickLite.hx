50d3c97b5ce74d68ba11012650ed9d6304f16152
GeoKureli <Gkurelic@gmail.com>
2015-01-06 07:02:20 -0600

    added Rick base class

--- source/com/geokureli/astley/art/RickLite.hx
+++ source/com/geokureli/astley/art/RickLite.hx
@@ -1,6 +1,7 @@
 package com.geokureli.astley.art;
 
-import com.geokureli.astley.AssetPaths.ImagePaths;
+import com.geokureli.astley.data.AssetPaths;
+import com.geokureli.astley.data.AssetPaths.ImagePaths;
 import flixel.FlxSprite;
 
 /**
@@ -8,15 +9,18 @@ import flixel.FlxSprite;
  * @author George
  */
 class RickLite extends FlxSprite{
-
-	static public inline var SPRITE:String = ImagePaths.PATH + "rick.png";
 	
 	static public inline var WIDTH:Int = 16;
 	
 	public function new(x:Float = 0, y:Float = 0) {
 		super(x, y);
 		
-		loadGraphic(SPRITE, true, WIDTH, 32);
+		loadGraphic(
+			AssetPaths.auto("rick"),
+			true,
+			WIDTH,
+			32
+		);
 		
 		var fartFrames:Array<Int> = [];
 		for (i in 1 ... frames - 1) {
