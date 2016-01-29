588f9846440133111559ff547732c5f18a123bf7
GeoKureli <Gkurelic@gmail.com>
2016-01-28 18:02:40 -0800

    begin credits layout

---
+++ source/com/geokureli/astley/art/ui/Credits.hx
@@ -0,0 +1,15 @@
+package com.geokureli.astley.art.ui;
+
+import flixel.group.FlxGroup;
+
+/**
+ * ...
+ * @author George
+ */
+class Credits extends FlxGroup{
+
+	public function new() {
+		
+		super();
+	}
+}
