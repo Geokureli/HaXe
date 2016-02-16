c2a5f07621f12232f0f4761c72fe30178775ede0
GeoKureli <Gkurelic@gmail.com>
2016-02-15 16:34:38 -0800

    added Dame reader, for Dame->Krakel mediation

---
+++ source/com/geokureli/krakel/data/serial/DameReader.hx
@@ -0,0 +1,166 @@
+package com.geokureli.krakel.data.serial;
+
+import com.geokureli.krakel.art.Sprite;
+import com.geokureli.krakel.components.Component;
+import com.geokureli.krakel.components.ComponentList;
+import com.geokureli.krakel.debug.Assert;
+import flash.geom.Point;
+import flash.geom.Rectangle;
+import flixel.FlxBasic;
+import flixel.group.FlxGroup;
+import flixel.group.FlxTypedGroup;
+import flixel.text.FlxText;
+
+typedef LookupMap = Map<String, Map<String, String>>;
+
+/**
+ * Deserializer for Krakel's main DAME json exporter for HaxeFlixel.
+ * @author George
+ */
+class DameReader extends Deserializer {
+	
+	var _lookups:LookupMap;
+	var _tempLookups:LookupMap;
+	var deserializer:Deserializer;
+	
+	public function new() {
+		super();
+		
+		setDefaults();
+		
+		addDefaultLookups();
+		addIterableAdders();
+	}
+	
+	function setDefaults() {
+		
+		_lookups = new Map<String, Map<String, String>>();
+		_tempLookups = new Map<String, Map<String, String>>();
+		
+		addPreParser(preParser);
+	}
+	
+	function preParser(data:Dynamic):Dynamic {
+		
+		var replacedFields = searchLookups(_tempLookups, data);
+		searchLookups(_lookups, data, replacedFields);
+		
+		//Todo: children[]
+		
+		return data;
+	}
+	
+	function searchLookups(lookups:LookupMap, data:Dynamic, ?excludedFields:Array<String>):Array<String> {
+		
+		if (excludedFields == null) excludedFields = [];
+		
+		var value:Dynamic;
+		for (field in lookups.keys()) {
+			
+			if (excludedFields.indexOf(field) == -1) {
+				
+				value = Reflect.field(data, field);
+				
+				if (lookups[field].exists(value))
+				{
+					Reflect.setProperty(data, field, lookups[field][value]);
+					
+					excludedFields.push(field);
+				}
+			}
+		}
+		
+		return excludedFields;
+	}
+	
+	public function addLookup(field:String, key:String, value:String):Void {
+		
+		if (!_lookups.exists(field))
+			_lookups[field] = new Map<String, String>();
+		
+		_lookups[field][key] = value;
+	}
+	
+	inline function addTempLookup(field:String, key:String, value:String):Void {
+		
+		if (!_tempLookups.exists(field))
+			_tempLookups[field] = new Map<String, String>();
+		
+		_tempLookups[field][key] = value;
+	}
+	
+	public function addClassLookup(key:String, type:Class<Dynamic>):Void {
+		
+		if (!Assert.nonNull(type)) return;
+		
+		addLookup("class", key, Type.getClassName(type));
+	}
+	
+	public function addAutoClassLookup(type:Class<Dynamic>):Void {
+		
+		if (!Assert.nonNull(type)) return;
+		
+		addClassLookup(Type.getClassName(type).split(".").pop(), type);
+	}
+	
+	public function createLevel<T>(data:Dynamic):Null<T> {
+		
+		if (!Reflect.hasField(data, "class") && Reflect.hasField(data, "level")) {
+			
+			if (Reflect.hasField(data, "data"))
+				parseData(data.data);
+			
+			data = data.level;
+		}
+		
+		return create(data);
+	}
+	
+	function parseData(data:Dynamic) {
+		
+		if (Reflect.hasField(data, "lookups")) {
+			
+			var lookupMap:Dynamic;
+			for (field in Reflect.fields(data.lookups)) {
+				
+				lookupMap = Reflect.field(data.lookups, field);
+				for (key in Reflect.fields(lookupMap)) {
+					
+					addLookup(field, key, Reflect.field(lookupMap, key));
+				}
+			}
+		}
+	}
+	
+	// =============================================================================
+	//{ region							INIT LIST
+	// =============================================================================
+	
+	inline function addDefaultLookups() {
+		
+		addAutoClassLookup(Point);
+		addAutoClassLookup(Rectangle);
+		addAutoClassLookup(Sprite);
+		
+		addClassLookup("Text", FlxText);
+	}
+	
+	inline function addIterableAdders() {
+		
+		addIterableHandler(FlxTypedGroup, addToFlxGroup);
+		addIterableHandler(ComponentList, addToComponentList);
+	}
+	
+	function addToFlxGroup(group:FlxTypedGroup<Dynamic>, item:FlxBasic):Void {
+		
+		group.add(item);
+	}
+	
+	function addToComponentList(list:ComponentList, component:Component):Void {
+		
+		list.add(component);
+	}
+	
+	//} endregion						INIT LIST
+	// =============================================================================
+}
