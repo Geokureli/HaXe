65a4508693b1fcb5953450485af4488339df70c9
GeoKureli <Gkurelic@gmail.com>
2016-06-26 03:12:35 -0700

    add special animation parser for Sprite

--- source/com/geokureli/krakel/art/Sprite.hx
+++ source/com/geokureli/krakel/art/Sprite.hx
@@ -3,19 +3,38 @@ package com.geokureli.krakel.art;
 import com.geokureli.krakel.components.Component;
 import com.geokureli.krakel.components.ComponentList;
 import com.geokureli.krakel.components.IComponentHolder;
+import com.geokureli.krakel.data.AssetPaths;
+import com.geokureli.krakel.data.serial.Deserializer;
+import com.geokureli.krakel.data.serial.IDeserializable;
+import com.geokureli.krakel.debug.Assert;
+import com.geokureli.krakel.interfaces.INamed;
 import com.geokureli.krakel.interfaces.IPoint;
+import com.geokureli.krakel.interfaces.IUpdate;
 import flixel.FlxBasic;
 import flixel.FlxObject;
 import flixel.FlxSprite;
+import openfl.Assets;
+
+typedef AnimationData = { graphic:String, ?width:Int, ?height:Int, ?unique:Bool, ?key:String };
 
 /**
  * ...
  * @author George
  */
 
-class Sprite extends FlxSprite implements IComponentHolder implements IPoint {
-
+class Sprite extends FlxSprite
+	implements IComponentHolder
+	implements IPoint
+	implements INamed
+	implements IDeserializable
+	implements IUpdate {
+	
 	public var components:ComponentList;
+	public var name:String;
+	public var tags:Array<String>;
+	/** A lookup for special parsers for specific, the callback returns true if the 
+	 * item was parsed successfully, otherwise the Deserializer will handle it*/
+	public var specialParsers:Map<String, FieldParser>;
 	
 	public function new(x:Float=0, y:Float=0, ?simpleGraphic:Dynamic) {
 		super(x, y, simpleGraphic);
@@ -27,6 +46,47 @@ class Sprite extends FlxSprite implements IComponentHolder implements IPoint {
 	function setDefaults():Void {
 		
 		components = new ComponentList();
+		tags = new Array<String>();
+		specialParsers = [
+			'graphic'   => deserializeGraphic,
+			'animation' => deserializeAnimation
+		];
+	}
+	
+	function deserializeGraphic(deserializer:Deserializer, value:Dynamic):Bool {
+		
+		if(Assert.is(value, String))
+		{
+			loadGraphic(AssetPaths.image(value));
+			
+			return true;
+		}
+		return false;
+	}
+	
+	function deserializeAnimation(deserializer:Deserializer, value:Dynamic):Bool {
+		
+		if (Assert.isObject(value, "Sprite: cannot parse graphic object")
+		 && Assert.has(value, 'graphic'))
+		{
+			value = deserializer.preParse(value);//TODO:Pre-parse in Deserializer before it gets here
+			var fullPath:String = AssetPaths.image(value.graphic);
+			if(Assert.isTrue(Assets.exists(fullPath, AssetType.IMAGE), "Invalid image " + fullPath))
+			{
+				loadGraphic(
+					fullPath,
+					true,
+					Reflect.hasField(value, 'width' ) ? value.width  : 0,
+					Reflect.hasField(value, 'height') ? value.height : 0,
+					Reflect.hasField(value, 'unique') ? value.unique : false,
+					Reflect.hasField(value, 'unique') ? value.unique : null
+				);
+			}
+			
+			return true;
+		}
+		
+		return false;
 	}
 	
 	function addAnimations() { }
@@ -65,5 +125,7 @@ class Sprite extends FlxSprite implements IComponentHolder implements IPoint {
 		super.destroy();
 		
 		components.destroy();
+		specialParsers = null;
+		tags = null;
 	}
 }
