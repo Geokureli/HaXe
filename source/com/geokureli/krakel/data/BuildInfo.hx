470fb0edd93a257bad837bc3d6a635d1fbd772fe
GeoKureli <Gkurelic@gmail.com>
2016-06-26 00:51:00 -0700

    whitespace

--- source/com/geokureli/krakel/data/BuildInfo.hx
+++ source/com/geokureli/krakel/data/BuildInfo.hx
@@ -7,24 +7,15 @@ package com.geokureli.krakel.data;
 class BuildInfo{
 
 	static public inline var BUILD_TARGET:String = 
-	#if flash
-		"flash";
-	#elseif ios
-		"ios";
-	#elseif android
-		"android";
-	#elseif windows
-		"windows";
-	#elseif mac
-		"mac";
-	#elseif linux
-		"linux";
-	#elseif html5
-		"html5";
-	#elseif js 
-		"js";
-	#else
-		"";
+	#if flash         "flash";
+	#elseif ios       "ios";
+	#elseif android   "android";
+	#elseif windows   "windows";
+	#elseif mac       "mac";
+	#elseif linux     "linux";
+	#elseif html5     "html5";
+	#elseif js        "js";
+	#else             "";
 	#end
 	
 	static public var INPUT_TYPE:String =
@@ -40,57 +31,24 @@ class BuildInfo{
 		
 		buildInfo = "Compilational constants: ";
 		var flags:Array<String> = [
-		#if web
-		"web",
-		#end
-		#if mobile
-		"mobile",
-		#end
-		#if desktop
-		"desktop",
-		#end
-		#if native
-		"native",
-		#end
-		#if ios
-		"ios",
-		#end
-		#if android
-		"android",
-		#end
-		#if blackberry
-		"blackberry",
-		#end
-		#if webos
-		"webos",
-		#end
-		#if windows
-		"windows",
-		#end
-		#if mac
-		"mac",
-		#end
-		#if linux
-		"linux",
-		#end
-		#if html5
-		"html5",
-		#end
-		#if flash
-		"flash",
-		#end
-		#if cpp
-		"cpp",
-		#end
-		#if neko
-        "neko",
-		#end
-		#if js
-		"js",
-		#end
-		""];
+			#if web        "web"       , #end
+			#if mobile     "mobile"    , #end
+			#if desktop    "desktop"   , #end
+			#if native     "native"    , #end
+			#if ios        "ios"       , #end
+			#if android    "android"   , #end
+			#if blackberry "blackberry", #end
+			#if webos      "webos"     , #end
+			#if windows    "windows"   , #end
+			#if mac        "mac"       , #end
+			#if linux      "linux"     , #end
+			#if html5      "html5"     , #end
+			#if flash      "flash"     , #end
+			#if cpp        "cpp"       , #end
+			#if neko       "neko"      , #end
+			#if js         "js"        , #end
 		
-		flags.pop();
+		null]; flags.pop();// --- ALL ITEMS HAVE COMMAS, ADD EMPTY, THEN REMOVE
 		
 		buildInfo += flags.join(", ");
 	}
