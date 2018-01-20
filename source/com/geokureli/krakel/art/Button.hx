8ed7f182d09b086c316db9241fb52b021ba9acec
George FunBook <gkurelic@gmail.com>
2018-01-19 19:27:31 -0600

    add buttoncomponent delegates

--- source/com/geokureli/krakel/art/Button.hx
+++ source/com/geokureli/krakel/art/Button.hx
@@ -1,5 +1,6 @@
 package com.geokureli.krakel.art;
 
+import flixel.util.FlxSignal;
 import com.geokureli.krakel.components.art.ButtonComponent;
 import com.geokureli.krakel.data.serial.Deserializer;
 import com.geokureli.krakel.debug.Assert;
@@ -11,11 +12,28 @@ import openfl.Lib;
  * @author George
  */
 class Button extends Sprite {
+
+	/** Shows the current state of the button, either `FlxButton.NORMAL`, `FlxButton.HIGHLIGHT` or `FlxButton.PRESSED`. */
+	public var state(get, never):Int;
+	/** The properties of this button's `onUp` event (callback function, sound). */
+	public var onUp(get, never):FlxSignal;
+	/** The properties of this button's `onDown` event (callback function, sound). */
+	public var onDown(get, never):FlxSignal;
+	/** The properties of this button's `onOver` event (callback function, sound). */
+	public var onOver(get, never):FlxSignal;
+	/** The properties of this button's `onOut` event (callback function, sound). */
+	public var onOut(get, never):FlxSignal;
+	/** Dispatched whenever the state changes */
+	public var onChange (default, null):FlxTypedSignal<Int->Void>;
 	
 	var _clickEReg:EReg = ~/^(.+?):(.+)/;
 	var _buttonComponent(default, null):ButtonComponent;
 	
-	public function new(x:Float = 0, y:Float = 0, ?simpleGraphic:Dynamic) { super(x, y, simpleGraphic); }
+	public function new(x:Float = 0, y:Float = 0, ?simpleGraphic:Dynamic, ?OnClick:Void->Void) {
+		super(x, y, simpleGraphic);
+		
+		_buttonComponent.onUp.add(OnClick);
+	}
 	
 	override function setDefaults():Void {
 		super.setDefaults();
@@ -62,4 +80,13 @@ class Button extends Sprite {
 		
 		return false;
 	}
+	
+	public function get_state():Int { return _buttonComponent.state; }
+	
+	public function get_onUp  ():FlxSignal { return _buttonComponent.onUp  ; }
+	public function get_onDown():FlxSignal { return _buttonComponent.onDown; }
+	public function get_onOver():FlxSignal { return _buttonComponent.onOver; }
+	public function get_onOut ():FlxSignal { return _buttonComponent.onOut ; }
+	
+	public function get_onChange():FlxTypedSignal<Int->Void> { return _buttonComponent.onChange; }
 }
