8e3c01481387d231a0ba0ed6a31fc206bc6ce657
George FunBook <gkurelic@gmail.com>
2018-02-22 23:44:30 -0600

    copy ash classMap

--- Krakel/source/com/geokureli/krakel/components/ComponentList.hx
+++ Krakel/source/com/geokureli/krakel/components/ComponentList.hx
@@ -1,6 +1,6 @@
 package com.geokureli.krakel.components;
 
-import ash.ClassMap;
+//import ash.ClassMap;
 
 /**
  * ...
@@ -89,5 +89,54 @@ class ComponentList {
         
         while(_list.length > 0)
             _list.shift().destroy();
+    }
+}
+
+private class ClassMap<K:Class<Dynamic>, V> implements Map.IMap<K, V>
+{
+    var h:haxe.ds.StringMap<V>;
+    
+    public inline function new():Void
+    {
+        h = new haxe.ds.StringMap<V>();
+    }
+    
+    public inline function get(k:K):Null<V>
+    {
+        return h.get(Type.getClassName(k));
+    }
+    
+    public inline function set(k:K, v:V):Void
+    {
+        h.set(Type.getClassName(k), v);
+    }
+    
+    public inline function exists(k:K):Bool
+    {
+        return h.exists(Type.getClassName(k));
+    }
+    
+    public inline function remove(k:K):Bool
+    {
+        return h.remove(Type.getClassName(k));
+    }
+    
+    public inline function keys():Iterator<K>
+    {
+        var i = h.keys();
+        return {
+            hasNext: i.hasNext,
+            next: function():K { return cast Type.resolveClass(i.next()); }
+        };
+    }
+    
+    public inline function iterator():Iterator<V>
+    {
+        return h.iterator();
+    }
+    
+    public inline function toString():String
+    {
+        return h.toString();
     }
 }
