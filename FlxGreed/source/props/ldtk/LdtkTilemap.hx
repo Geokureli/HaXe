9ce9192fea670ab9e2e02ab355309b52bc5cfb15
George FunBook <gkurelic@gmail.com>
2024-06-18 18:06:16 -0500

    5.9.0 upkeep

--- FlxGreed/source/props/ldtk/LdtkTilemap.hx
+++ FlxGreed/source/props/ldtk/LdtkTilemap.hx
@@ -1,7 +1,7 @@
 package props.ldtk;
 
-import data.Ldtk;
 import ldtk.Json;
+import ldtk.Layer_Tiles;
 import ldtk.Tileset;
 import flixel.FlxObject;
 import flixel.math.FlxMath;
@@ -60,56 +60,6 @@ class LdtkTypedTilemap<Tag:EnumValue, Tile:LdtkTile<Tag>> extends FlxTypedTilema
         LdtkTileLayerTools.initTilemap(layer, this, (i, md, _)->handleTileMetadata(i, md), (i, tags, _)->handleTileTags(i, tags));
     }
     
-    public function forEachOverlappingTile(object:FlxObject, func:(LdtkTile<Tag>)->Void, ?position:FlxPoint)
-    {
-        var xPos = x;
-        var yPos = y;
-        
-        if (position != null)
-        {
-            xPos = position.x;
-            yPos = position.y;
-            position.putWeak();
-        }
-        
-        inline function bindInt(value:Int, min:Int, max:Int)
-        {
-            return Std.int(FlxMath.bound(value, min, max));
-        }
-        
-        // Figure out what tiles we need to check against, and bind them by the map edges
-        final minTileX:Int = bindInt(Math.floor((object.x - xPos) / scaledTileWidth), 0, widthInTiles);
-        final minTileY:Int = bindInt(Math.floor((object.y - yPos) / scaledTileHeight), 0, heightInTiles);
-        final maxTileX:Int = bindInt(Math.ceil((object.x + object.width - xPos) / scaledTileWidth), 0, widthInTiles);
-        final maxTileY:Int = bindInt(Math.ceil((object.y + object.height - yPos) / scaledTileHeight), 0, heightInTiles);
-        
-        // Loop through the range of tiles and call the callback on them, accordingly
-        final deltaX:Float = xPos - last.x;
-        final deltaY:Float = yPos - last.y;
-        
-        for (row in minTileY...maxTileY)
-        {
-            for (column in minTileX...maxTileX)
-            {
-                final mapIndex:Int = (row * widthInTiles) + column;
-                final dataIndex:Int = _data[mapIndex] < 0 ? 0 : _data[mapIndex];
-                
-                final tile = _tileObjects[dataIndex];
-                tile.width = scaledTileWidth;
-                tile.height = scaledTileHeight;
-                tile.x = xPos + column * tile.width;
-                tile.y = yPos + row * tile.height;
-                tile.last.x = tile.x - deltaX;
-                tile.last.y = tile.y - deltaY;
-                
-                if (tile.overlapsObject(object))
-                {
-                    func(tile);
-                }
-            }
-        }
-    }
-    
     public function overlapsTag(object, tag, ?position):Bool
     {
         return overlapsTagWithCallback(object, tag, null, position);
@@ -117,22 +67,21 @@ class LdtkTypedTilemap<Tag:EnumValue, Tile:LdtkTile<Tag>> extends FlxTypedTilema
     
     public function overlapsTagWithCallback(object, tag:Tag, ?callback:(LdtkTile<Tag>)->Void, ?position):Bool
     {
-        var overlapFound = false;
         function checkTags(tile:LdtkTile<Tag>)
         {
             if (tile.hasTag(tag))
             {
-                overlapFound = true;
+                if (callback != null)
+                    callback(tile);
+                
+                return true;
             }
             
-            if (callback != null)
-                callback(tile);
+            return false;
         }
         
-        forEachOverlappingTile(object, checkTags, position);
-        return overlapFound;
+        return isOverlappingTile(object, checkTags, position);
     }
-    
     
     function handleTileMetadata(index:Int, metaData:String)
     {
@@ -161,13 +110,15 @@ abstract LdtkTileLayerTools(Tileset) from Tileset
         final tiles = createTileArray(layer);
         // final graphic = getPath(layer.tileset);
         final graphic = data.Global.getMainGraphic();
-        final tileWidth = layer.tileset.tileGridSize;
-        final tileHeight = layer.tileset.tileGridSize;
+        @:privateAccess
+        final tileset:Tileset = layer.untypedTileset;
+        final tileWidth = tileset.tileGridSize;
+        final tileHeight = tileset.tileGridSize;
         tilemap.loadMapFromArray(tiles, layer.cWid, layer.cHei, graphic, tileWidth, tileHeight, null, 0, 0);
         
         if (metadataHandler != null)
         {
-            for (tileData in layer.tileset.json.customData)
+            for (tileData in tileset.json.customData)
             {
                 metadataHandler(tileData.tileId, tileData.data, tilemap);
             }
@@ -178,9 +129,17 @@ abstract LdtkTileLayerTools(Tileset) from Tileset
             @:privateAccess
             for (i in 0...tilemap._tileObjects.length)
             {
-                tagHandler(i, cast layer.tileset.getAllTags(i), tilemap);
+                final tags = getAllTags(tileset, i);
+                tagHandler(i, tags, tilemap);
             }
         }
+    }
+    
+    static function getAllTags<Tag:EnumValue>(tileset:Tileset, tileId:Int):Array<Tag>
+    {
+        // just assume it had the getAllTags method generated
+        final castTileset:{ getAllTags:(Int)->Array<Tag> } = cast tileset;
+        return castTileset.getAllTags(tileId);
     }
     
     static public function createTileArray(layer:Layer_Tiles)
